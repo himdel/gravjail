5e8076242072ef2b5cacbcfea335ba6265106741
Martin Hradil <himdel@seznam.cz>
2010-08-24 03:29:53 +0000

    drzeni klaves

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ from pygame.locals import *
 
 import universe
 import viewport
+import player
 
 fps = 100
 dt = 1.0/fps
 clk = pygame.time.Clock()
 
 x = True
+players = [player.Player(universe.s)]
+keys = {}
 while x:
 	events = pygame.event.get()
 	for e in events:
-		if e.type==QUIT:
+		if e.type == QUIT:
 			x = False
-		if e.type==KEYDOWN:
-			x = False
+		if e.type == KEYDOWN:
+			if e.key == K_ESCAPE:
+				x = False
+			keys[e.key] = 1
+		if e.type == KEYUP:
+			del keys[e.key]
+
+	for p in players:
+		for k in keys.keys():
+			p.process_key(k)
 
 	universe.step(dt)
 
