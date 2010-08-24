cfc401b01d84a7804338319bb209fb46c3c98503
Martin Hradil <himdel@seznam.cz>
2010-08-24 03:30:18 +0000

    ship controls

--- ship.py
+++ ship.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import ode
 import pygame
+from math import *
+from random import random
 from universe import world, space
 from viewport import surface
 
@@ -10,6 +12,7 @@ class Ship:
 	def __init__(self, x, y):
 		self.body = b = ode.Body(world)
 		self.mass = M = ode.Mass()
+		self.angle = random() * 2 * pi
 		M.setSphere(250, 0.05)
 		b.setMass(M)
 		b.setPosition((x, y, 0))
@@ -20,3 +23,15 @@ class Ship:
 	def paint(self, coord, ra):
 		x, y, z = self.body.getPosition()
 		pygame.draw.circle(surface, (0, 0, 255), coord(x, y), ra(0.05), 0)
+		fx = cos(self.angle) * 0.05
+		fy = sin(self.angle) * 0.05
+		pygame.draw.circle(surface, (255, 0, 0), coord(x + fx, y + fy), ra(0.02), 0)
+
+	def acc(self, force):
+		fx = cos(self.angle) * force
+		fy = sin(self.angle) * force
+		print fx, fy
+		self.body.addForce((fx, fy, 0))
+	
+	def turn(self, angle):
+		self.angle += angle * 0.1
