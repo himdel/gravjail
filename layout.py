eca1f85463167775644eee694d57ef9d371b643f
Martin Hradil <himdel@seznam.cz>
2010-08-26 02:29:13 +0000

    layout blit testing

--- layout.py
+++ layout.py
@@ -12,16 +12,22 @@ class Layout:
 
 #		# 3 viewporty, krasa
 #		for p in players:
-#			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 170))
-#			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 13.6, stars = False))
-#			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 3.4, stars = False))
+#			self.vps.append(Viewport(p, xres, yres, zoom = 170))
+#			self.vps.append(Viewport(p, xres, yres, zoom = 13.6, stars = False))
+#			self.vps.append(Viewport(p, xres, yres, zoom = 3.4, stars = False))
 
 		for p in players:	# test 1 player, 2 pod sebou vp
-			self.vps.append(Viewport(p, xres / 2 - 1, yres, ox = 0, oy = 0, zoom = 170))
-			self.vps.append(Viewport(p, xres / 2 - 1, yres, ox = xres / 2 + 1, oy = 0, zoom = 170))
+			self.vps.append((
+				Viewport(p, xres / 2 - 1, yres, zoom = 170),
+			))
+			self.vps.append((
+				Viewport(p, xres / 2 - 1, yres, zoom = 170),
+			))
 
 	def drawLayout(self, arg):
 		surface.fill((0, 0, 0))
 		for vp in self.vps:
-			vp.paint(arg)
+			vpa, vpb = vp
+			vpa.paint(arg)
+			surface.blit(vpa.surface, vpb)
 		pygame.display.flip()
