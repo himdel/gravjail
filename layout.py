bf73a517a1a20e1f506d0260b8035521bad9fbd5
Martin Hradil <himdel@seznam.cz>
2010-08-26 02:39:41 +0000

    fce na pridani viewportu pro kazdeho playera

--- layout.py
+++ layout.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python
 import pygame
+from pygame.locals import *
 
 from viewport import Viewport, surface
 from consts import *
 
+def add_p(vps, player, w, h, l, r):
+	vps += [
+			((
+				Viewport(player, w, h, zoom = 170),
+				pygame.Rect(l, r, w, h),
+			)),
+			((
+				Viewport(player, w, h, zoom = 13.6, stars = False),
+				pygame.Rect(l, r, w, h),
+			)),
+			((
+				Viewport(player, w, h, zoom = 3.4, stars = False),
+				pygame.Rect(l, r, w, h),
+			)),
+	]
+
 class Layout:
-	
+
 	def __init__(self, players):
 		self.players = players
 		self.vps = []
 
-#		# 3 viewporty, krasa
-#		for p in players:
-#			self.vps.append(Viewport(p, xres, yres, zoom = 170))
-#			self.vps.append(Viewport(p, xres, yres, zoom = 13.6, stars = False))
-#			self.vps.append(Viewport(p, xres, yres, zoom = 3.4, stars = False))
-
 		for p in players:	# test 1 player, 2 pod sebou vp
-			self.vps.append((
-				Viewport(p, xres / 2 - 1, yres, zoom = 170),
-			))
-			self.vps.append((
-				Viewport(p, xres / 2 - 1, yres, zoom = 170),
-			))
+			add_p(self.vps, p, xres / 2 - 1, yres / 2 - 1, 0, 0)
+			add_p(self.vps, p, xres / 2 - 1, yres / 2 - 1, xres / 2 + 1, 0)
+			add_p(self.vps, p, xres / 2 - 1, yres / 2 - 1, 0, yres / 2 + 1)
+			add_p(self.vps, p, xres / 2 - 1, yres / 2 - 1, xres / 2 + 1, yres / 2 + 1)
+
 
 	def drawLayout(self, arg):
 		surface.fill((0, 0, 0))
 		for vp in self.vps:
 			vpa, vpb = vp
 			vpa.paint(arg)
-			surface.blit(vpa.surface, vpb)
+			surface.blit(vpa.surface, vpb, None, BLEND_ADD)
 		pygame.display.flip()
