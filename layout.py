a069f1618f770d7cfea7663c12e1be889b36a8ec
Martin Hradil <himdel@seznam.cz>
2010-08-26 02:19:13 +0000

    map viewports commented, now testing side by side

--- layout.py
+++ layout.py
@@ -9,8 +9,16 @@ class Layout:
 	def __init__(self, players):
 		self.players = players
 		self.vps = []
-		for p in players:
-			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 170))
+
+#		# 3 viewporty, krasa
+#		for p in players:
+#			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 170))
+#			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 13.6, stars = False))
+#			self.vps.append(Viewport(p, xres, yres, ox = 0, oy = 0, zoom = 3.4, stars = False))
+
+		for p in players:	# test 1 player, 2 pod sebou vp
+			self.vps.append(Viewport(p, xres / 2 - 1, yres, ox = 0, oy = 0, zoom = 170))
+			self.vps.append(Viewport(p, xres / 2 - 1, yres, ox = xres / 2 + 1, oy = 0, zoom = 170))
 
 	def drawLayout(self, arg):
 		surface.fill((0, 0, 0))
