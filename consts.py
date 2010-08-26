94cc72e20299da9ee6d59f9a879ea9b4fbfe223d
Martin Hradil <himdel@seznam.cz>
2010-08-26 04:53:36 +0000

    hole, hugs colors, bounds

--- consts.py
+++ consts.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
 # global constants
-xres = 640
-yres = 480
+xres = 512 + 384
+yres = xres / 4 * 3
 
 holes_spc = 128
 holes_num = 16
 
-ship_acc = 0.1
+ship_acc = 0.2
+
+hole_mass = 50000
+hole_r = 0.3			# active hole radius
+hole_w = 0.5		# visual hole radius
+
+bounds_acc = 0.1	# * distance in
+
+huds_force = (255, 0, 0)
+huds_vel = (0, 255, 0)
