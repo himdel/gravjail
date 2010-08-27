98ed2ca726f1d9740ab72744917a6619fa231dc2
Martin Hradil <himdel@seznam.cz>
2010-08-27 22:04:01 +0000

    graphics loading and scaling

---
+++ graphics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import pygame
+
+blackhole = None
+ship = None
+bonus = None
+
+
+enabled = False
+
+def toggle():
+	global enabled, blackhole
+	if enabled:
+		enabled = False
+		blackhole = None
+	else:
+		enabled = True
+		blackhole = {0: pygame.image.load("pic/blackhole.png")}
+
+def scale(obj, side):
+	if not side in obj:
+		obj[side] = pygame.transform.scale(obj[0], (side, side))
+	return obj[side]
