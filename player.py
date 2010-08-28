505c3e5416654d3cb6e38822eb08195d1ce2839f
Martin Hradil <himdel@seznam.cz>
2010-08-28 18:35:38 +0000

    players adds itself, has checkpoints, ignores controls if dead

--- player.py
+++ player.py
@@ -1,13 +1,20 @@
 from pygame.locals import *
 from consts import *
+from universe import players
 
 class Player:
+	checkpoints = 0
 
-	def __init__(self, ship, controls):
+	def __init__(self, ship, controls, name):
+		self.name = name
 		self.acc, self.brake, self.left, self.right = controls
 		self.ship = ship
+		self.ship.player = self
+		players.append(self)
 	
 	def process_key(self, key):
+		if self.ship.alive == False:
+			return
 		if key == self.brake:
 			self.ship.acc(-ship_acc)
 		elif key == self.acc:
