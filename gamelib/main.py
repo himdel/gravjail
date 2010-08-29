9ddba382d610a12f743acd46f4fa227d55719d98
Martin Hradil <himdel@seznam.cz>
2010-08-29 15:22:24 +0000

    main inits a lots of stuff now, show victory and loss screens

--- gamelib/main.py
+++ gamelib/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import ode
 import pygame
 from pygame.locals import *
 import random
@@ -19,29 +20,35 @@ dt = 1.0/fps
 clk = pygame.time.Clock()
 
 def play(nplayers):
+	universe.world = ode.World()
+	universe.space = ode.SimpleSpace()
 	universe.ships = []
 	universe.holes = []
 	universe.checkpoints = []
 	universe.players = []
+	my_ships = []
 
 	pos = [(1.5, -0.5), (1.5, -1.5), (0.5, -0.5), (0.5, -1.5)]
 	pnames = ["yellow", "violet", "cyan", "white"]
 
 	for x in range(nplayers):
-		s = ship.Ship(pos[x][0], pos[x][1], consts.pcolors[x])
+		s = ship.Ship(pos[x][0], pos[x][1], consts.pcolors[x], universe.ships, (universe.world, universe.space))
 		p = player.Player(s, consts.keyConfigs[x], pnames[x])
 		universe.ships.append(s)
+		my_ships.append(s)
 		universe.players.append(p)
 
 	for x in range(consts.holes_num):
-		h = Hole(random.random() * consts.holes_spc - consts.holes_spc / 2, random.random() * consts.holes_spc - consts.holes_spc / 2)
+		h = Hole(random.random() * consts.holes_spc - consts.holes_spc / 2, random.random() * consts.holes_spc - consts.holes_spc / 2, (universe.world, universe.space))
 		universe.holes.append(h)
 
 	for x in range(consts.checkpoints_num):
-		universe.checkpoints.append(Checkpoint(random.random() * consts.holes_spc - consts.holes_spc / 2, random.random() * consts.holes_spc - consts.holes_spc / 2))
+		c = Checkpoint(random.random() * consts.holes_spc - consts.holes_spc / 2, random.random() * consts.holes_spc - consts.holes_spc / 2, (universe.world, universe.space))
+		universe.checkpoints.append(c)
 
 	game_layout = layout.Layout(universe.players)
 	x = True
+	w = False
 	keys = {}
 	while x:
 		events = pygame.event.get()
@@ -73,11 +80,18 @@ def play(nplayers):
 
 		# game over
 		x = False
-		for s in universe.ships:
+		for s in my_ships:
 			if s.alive:
 				x = True
+			if s.winner:
+				w = True
 
 		clk.tick(fps)
+
+	if w:
+		menu.screen(menu.victory)
+	else:
+		menu.screen(menu.loss)
 
 
 
