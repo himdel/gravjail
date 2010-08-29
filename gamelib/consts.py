192ab34d3b75966e6abed8217146c8971afaf14d
Martin Hradil <himdel@seznam.cz>
2010-08-29 15:20:04 +0000

    const tweakage

--- gamelib/consts.py
+++ gamelib/consts.py
@@ -16,13 +16,14 @@ ship_acc = 0.2
 ship_mass = 250
 
 #hole_mass = 50000
-hole_mass = 100000
-hole_r = 0.3		# active hole radius
+hole_mass = 150000
+hole_r = 0.2		# active hole radius
 hole_w = 0.5		# visual hole radius
 
 bounds_acc = 0.1	# * distance in
 
-hfactor = 1000		# for health loss after collision
+#hfactor = 1000		# for health loss after collision
+hfactor = 0.1
 
 huds_force = (255, 0, 0)
 huds_vel = (0, 255, 0)
